0dad4674a68bfb537f72635a09f10abae6546aeb
Luis Inzunza <cruzalejandro113@gmail.com>
2022-10-02 16:42:33 -0500

    ignora primera linea de codigo y crea clientes

--- src/Banco.java
+++ src/Banco.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-
-
 public class Banco {
     private String Name;
     private Cliente clientes;
@@ -16,12 +14,21 @@ public class Banco {
 
     private String Datos(){
         try {
-            File f = new File("info.csv");
-            Scanner s = new Scanner(f);
-            while (s.hasNextLine()){
-                String line = s.nextLine();
-                System.out.println(line);
+            File archivo = new File("info.csv");
+            Scanner datos = new Scanner(archivo);
+            int x= 0;
+            while (datos.hasNextLine()){
+                String linea = datos.nextLine();
+                if(linea !=null && x!=0){
+                    String[] parte = linea.split(",");
+                    //asigno valores por cada datos que tenemos por linea
+                    Cliente nuevo = new Cliente(parte[1]); //se usa la parte donde se encuentra el nombre.
+                    System.out.println(linea);
+                    
+                }
+                x++;
             }
+            datos.close();
         }
 
 
